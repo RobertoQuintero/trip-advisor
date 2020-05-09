214aecde4ac59afee35fab4855c69316e6a0733c
Roberto Quintero <langdonhp435@gmail.com>
2020-05-09 13:03:46 -0500

    Termina sección lista de restaurantes

--- app/navigations/RestaurantsStack.js
+++ app/navigations/RestaurantsStack.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import Restaurants from "../screens/Restaurants/Restaurants";
 import AddRestaurant from "../screens/Restaurants/AddRestaurant";
+import Restaurant from "../screens/Restaurants/Restaurant";
 
 const Stack = createStackNavigator();
 
@@ -19,6 +20,7 @@ export default RestaurantsStack = () => {
         component={AddRestaurant}
         options={{ title: "Añadir nuevo Restaurante" }}
       />
+      <Stack.Screen name="restaurant" component={Restaurant} />
     </Stack.Navigator>
   );
 };
--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,22 +1,87 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Icon } from "react-native-elements";
-import { firebaseApp } from "../../utils/firebase";
+import { firebaseapp } from "../../utils/firebase";
 import firebase from "firebase/app";
+import "firebase/firestore";
+import ListRestaurants from "../../components/Restaurants/ListRestaurants";
+const db = firebase.firestore(firebaseapp);
+import { useFocusEffect } from "@react-navigation/native";
 
 const Restaurants = (props) => {
   const { navigation } = props;
   const [user, setUser] = useState(null);
-
+  const [restaurants, setRestaurants] = useState([]);
+  const [totalRestaurnts, setTotalRestaurnts] = useState(0);
+  const limitRestaurants = 10;
+  const [startRestaurants, setStartRestaurants] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     firebase.auth().onAuthStateChanged((userInfo) => {
       setUser(userInfo);
     });
   }, []);
 
+  useFocusEffect(
+    //de navigation al dirigir a esta vista
+    useCallback(() => {
+      //recarga al direccionar a esta pagina
+      db.collection("restaurants")
+        .get()
+        .then((snap) => {
+          setTotalRestaurnts(snap.size);
+        });
+
+      const resultRestaurants = [];
+      db.collection("restaurants")
+        .orderBy("createAt", "desc")
+        .limit(limitRestaurants)
+        .get()
+        .then((response) => {
+          setStartRestaurants(response.docs[response.docs.length - 1]);
+          response.forEach((doc) => {
+            const restaurant = doc.data();
+            restaurant.id = doc.id;
+            resultRestaurants.push(restaurant);
+          });
+          setRestaurants(resultRestaurants);
+        });
+    }, [])
+  );
+
+  //after list restaurants
+  const handleLoadMore = () => {
+    const resultRestaurants = [];
+    restaurants.length < totalRestaurnts && setIsLoading(true);
+
+    db.collection("restaurants")
+      .orderBy("createAt", "desc")
+      .startAfter(startRestaurants.data().createAt)
+      .limit(limitRestaurants)
+      .get()
+      .then((response) => {
+        if (response.docs.length > 0) {
+          setStartRestaurants(response.docs[response.docs.length - 1]);
+        } else {
+          setIsLoading(false);
+        }
+
+        response.forEach((doc) => {
+          const restaurant = doc.data();
+          restaurants.id = doc.id;
+          resultRestaurants.push(restaurant);
+        });
+        setRestaurants([...restaurants, ...resultRestaurants]);
+      });
+  };
+
   return (
     <View style={styles.viewBody}>
-      <Text>Restaurants</Text>
+      <ListRestaurants
+        restaurants={restaurants}
+        handleLoadMore={handleLoadMore}
+        isLoading={isLoading}
+      />
       {user && (
         <Icon
           reverse
